c7346b96a0c56f41cefe0c1b51a7b5c0fc588746
Andrew Lin <andrew.lin@ieee.org>
2015-01-18 15:07:25 -0800

    More documentation

--- README.md
+++ README.md
@@ -8,6 +8,27 @@ For unittests:
 
 * Mock (https://pypi.python.org/pypi/mock) (Python 3.2 only).
 
+## Design
+The flashcards application follows the MVC design pattern. User interaction
+is separated from the implementation so that the application can be easily
+ported between systems without a complete rewrite.
+
+So, for instance, a gtk front end could be written with a Quiz mixin to present
+the user with a GUI interface instead of the text-based one that flashcards.py
+presents. Or the Quiz class culd be wrapped inside a web API to create a 
+client-server-based interface.
+
+Flashcards.py is the application, presenting card questions and answers, and
+translating user input for the Quiz class.
+
+The Quiz class contains the 'smarts' of the game. It separates the cards into
+hard, medium, and easy questions based on the previous scores of the deck.
+
+The Deck class is responsible for holding and serving up Flashcards.
+
+The Flashcard class contains the question, answer, and statistics on how that
+question was answered and when.
+
 ## Deck File Format
 Deck files are csv files with some format requirements, so they can be created
 in your favorite spreadsheet software.
@@ -23,7 +44,7 @@ The special keywords are:
 * Name -- The name of the deck, e.g. Single Digit Addition
 * Quiz -- Specifies the beginning of the quiz section.
 
-### Quiz section
+### Quiz Section
 The quiz section begins with a header row describing the values in the row.
 It is defined to be 'Question, Answer, Attempts, Correct, Last Shown'.
 
@@ -43,6 +64,20 @@ correctly. This can be left blank if you are creating decks in a spreadsheet.
 The 'Last Shown' value is the ISO-format date-time string of the UTC time of
 the last time this question has been attempted. This ca be left blank if you
 are creating decks in a spreadsheet.
+
+### Sample Deck File
+Name: Sample Deck
+Quiz:
+Question,Answer,Attempts,Correct,Last Shown
+1 + 1, 2
+1 + 2, 3
+1 + 3, 4
+1 + 4, 5
+1 + 5, 6
+1 + 6, 7
+1 + 7, 8
+1 + 8, 9
+1 + 9, 10
 
 ## License
 This work is licensed under a Creative Commons
--- lib/tests/test_deck.py
+++ lib/tests/test_deck.py
@@ -12,7 +12,7 @@ from lib.deck import Deck, CardData
 from lib.flashcard import Flashcard
 
 
-class DeckTestCase(unittest.TestCase):
+class DeckCreationTestCase(unittest.TestCase):
     """Unittests for Deck class."""
     def test_creation(self):
         """Constructor tests."""
@@ -30,39 +30,63 @@ class DeckTestCase(unittest.TestCase):
             deck.add_card(c)
             self.assertEqual(len(deck), idx)
 
-    def test_iterate(self):
-        """Iteration tests."""
-        deck_name = 'test deck'
-        cards = ['card 0', 'card 1', 'card 2']
-        deck = Deck(deck_name)
-        for c in cards:
-            deck.add_card(c)
 
-        deck_cards = [c for c in deck]
-        self.assertEqual(frozenset(cards), frozenset(deck_cards))
-
-    def test_load_and_save(self):
-        """load() interface tests."""
-        # Build a deck.
-        deck = Deck('test deck')
-        cards = [
+class DeckManipulationTestCase(unittest.TestCase):
+    def setUp(self):
+        self.deck_name = 'test deck'
+        self.deck = Deck(self.deck_name)
+        self.cards = [
             Flashcard('q0', 'a0', 0, 0, str(datetime.datetime.utcnow())),
             Flashcard('q1', 'a1', 0, 0, str(datetime.datetime.utcnow())),
             Flashcard('q2', 'a2', 0, 0, str(datetime.datetime.utcnow()))
         ]
-        for c in cards:
-            deck.add_card(c)
+        for c in self.cards:
+            self.deck.add_card(c)
 
+    def test_iterate(self):
+        """Iteration tests."""
+        self.assertEqual(
+            frozenset(c for c in self.deck),
+            frozenset(self.deck_cards)
+        )
+
+    def test_load_and_save(self):
+        """load() and save() interface tests."""
         # Save and load the deck.
         with tempfile.NamedTemporaryFile(mode='w', newline='') as tf:
-            deck.save(tf.name, overwrite=True)
+            self.deck.save(tf.name, overwrite=True)
             saved_deck = Deck.load(tf.name)
 
         # Test.
-        self.assertEqual(saved_deck, deck)
+        self.assertEqual(saved_deck, self.deck)
+
+    def test_sample_deck_file(self):
+        """Load sample deck from README.md test."""
+        sample = """Name: Sample Deck
+Quiz:
+Question,Answer,Attempts,Correct,Last Shown
+1 + 1, 2
+1 + 2, 3
+1 + 3, 4
+1 + 4, 5
+1 + 5, 6
+1 + 6, 7
+1 + 7, 8
+1 + 8, 9
+1 + 9, 10
+"""
+        with tempfile.NamedTemporaryFile(mode='w', newline='') as tf:
+            with open(tf.name, 'w') as f:
+                f.write(sample)
+
+            saved_deck = Deck.load(tf.name)
+            self.assertEqual(saved_deck.name, 'Sample Deck')
+            self.assertEqual(len(saved_deck), 9)
 
     def test_answers(self):
         """answers() interface tests."""
+        expected_answers = frozenset([c.answer for c in self.cards])
+        self.assertEqual(frozenset(self.deck.answers()), expected_answers)
 
 
 if __name__ == '__main__':
