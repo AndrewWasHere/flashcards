490d969f1799df675d788939325a73938f10be87
Andrew Lin <andrew.lin@ieee.org>
2015-01-18 11:16:37 -0800

    Improved const test.

--- lib/tests/test_const.py
+++ lib/tests/test_const.py
@@ -11,22 +11,22 @@ from lib.data_types import Const
 
 class TestConsts(Const):
     """Constants to test."""
-    foo = 'foo'
-    bar = 'bar'
-    baz = 'baz'
+    foo = 'foo-ish'
+    bar = 'bar-ish'
+    baz = 'baz-ish'
 
 
 class ConstTestCase(unittest.TestCase):
     """Unittests for Const class."""
     def test_access(self):
         """Constant access tests."""
-        self.assertEqual(TestConsts.foo, 'foo')
-        self.assertEqual(TestConsts.bar, 'bar')
-        self.assertEqual(TestConsts.baz, 'baz')
+        self.assertEqual(TestConsts.foo, 'foo-ish')
+        self.assertEqual(TestConsts.bar, 'bar-ish')
+        self.assertEqual(TestConsts.baz, 'baz-ish')
 
     def test_all(self):
         """all() interface tests."""
-        consts = ['foo', 'bar', 'baz']
+        consts = ['foo-ish', 'bar-ish', 'baz-ish']
         for c in consts:
             self.assertTrue(c in TestConsts.all())
 
